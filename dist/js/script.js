b5e39d560ecd2dfd4ad544ac4b91790e21ec026a
Mohamed Adel <92227128+mo-adell@users.noreply.github.com>
2022-04-15 15:01:15 +0200

    finished modal section

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,1 +1,15 @@
-console.log("hello world")
+"use strict"
+
+const popup = {
+  showBtn : document.querySelectorAll('.cards-item'),
+  modal   : document.querySelector('.modal'),
+
+  show() {
+    this.modal.style.display = 'grid';
+  }
+}
+
+for (const item of popup.showBtn) {
+  item.addEventListener('click', _=> popup.show())
+}
+
